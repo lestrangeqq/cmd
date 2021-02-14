755be0f91ea5eb229944e5f8cbd913d1279acab0
Alex <lestrangeqq@gmail.com>
2021-02-14 23:12:29 +0200

    Update copy_file.bat

--- copy_file.bat
+++ copy_file.bat
@@ -1,2 +1,4 @@
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit ) REM не даем окну закрыться при возникновении ошибки
 COPY "C:\Users\админ\Saved Games\Diablo II\" "D:\games\Diablo II\save_backup" REM копирование из папки в папку
+
+REM если в пути встречается кирилица нужно перевести батник в кодировку OEM 866 и убедиться что в пути нет иероглифов(переписать содержимое иероглифов).
