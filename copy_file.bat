7ade12cdc651fa6f061ecb2d5f0504d56a3b9dc0
Alex <lestrangeqq@gmail.com>
2021-02-14 23:11:01 +0200

    Create copy_file.bat

---
+++ copy_file.bat
@@ -0,0 +1,2 @@
+if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit ) REM не даем окну закрыться при возникновении ошибки
+COPY "C:\Users\админ\Saved Games\Diablo II\" "D:\games\Diablo II\save_backup" REM копирование из папки в папку
